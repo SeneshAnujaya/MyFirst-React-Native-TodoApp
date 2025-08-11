224bed7fd0535b48ef7788ad27f0c7a60ad207ff
SeneshAnujaya <senesh9637@gmail.com>
2025-08-11 19:11:11 +0530

    implement dark light mode

---
+++ data/todos.js
@@ -0,0 +1,7 @@
+export const data = [
+  { id: 1, title: "Buy groceries", completed: false },
+  { id: 2, title: "Walk the dog", completed: true },
+  { id: 3, title: "Read a book", completed: false },
+  { id: 4, title: "Clean the room", completed: true },
+  { id: 5, title: "Write some code", completed: false }
+];
